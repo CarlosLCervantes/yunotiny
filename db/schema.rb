a84b37fa96d1e67e1e0d9aa14abc24155c402236
Edoardo <edoardo.conti@gmail.com>
2014-03-22 22:08:01 -0700

    Update Gemfile.lock for Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@
 ActiveRecord::Schema.define(version: 20140322231916) do
 
   create_table "urls", force: true do |t|
-    t.string   "url",        null: false
+    t.string   "link",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
